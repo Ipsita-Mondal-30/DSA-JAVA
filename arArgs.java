4c52ee4b0b07f720aa21ee15ea9230c4dbfcab99
Ipsita Mondal <ipsitaamondal@gmail.com>
2025-02-19 11:21:44 +0530

    questions

---
+++ arArgs.java
@@ -0,0 +1,10 @@
+public class arArgs {
+    public static void main(String[] args) {
+        int result = sum(3, 4);
+        System.out.println("Sum: " + result);
+    }
+
+    static int sum(int a, int b) {
+        return a + b;
+    }
+}
